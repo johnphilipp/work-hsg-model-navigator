15c7f4742cf48879893578bb551316d2c807bf1a
Philipp John <35657453+johnphilipp@users.noreply.github.com>
2023-01-30 12:03:40 +0100

    changed server ip address

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -9,7 +9,7 @@ import { Logo } from "../components/utils/Logo";
 
 async function loginUser(credentials) {
   console.log("post");
-  return fetch("http://localhost:8000/login/", {
+  return fetch("http://127.0.0.1:8000/login/", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
