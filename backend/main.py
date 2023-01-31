accfcb4fd383421d99a57ba1714096d622ab02f2
Philipp John <35657453+johnphilipp@users.noreply.github.com>
2023-01-31 23:07:36 +0100

    added new api routes to fetch mock data

--- backend/main.py
+++ backend/main.py
@@ -1,13 +1,22 @@
 from fastapi import FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
-from typing import Union
+from typing import List
 import json
 
 
 class Login(BaseModel):
     email: str
     password: str
+
+
+class Models(BaseModel):
+    categories: List[str]
+
+
+class Model(BaseModel):
+    category: str
+    name: str
 
 
 app = FastAPI()
@@ -45,4 +54,28 @@ def login(login: Login):
         return HTTPException(status_code=401, detail="Password incorrect")
     else:
         print("Credentials correct")
-        return {"token": "123-456-789"}
+        return {"token": "123-456-7890"}
+
+
+@app.get("/categories/")
+def categories():
+    models_data = json.load(open("models_data.json"))
+    out = [x["category"] for x in models_data]
+    return {"categories": out}
+
+
+@ app.post("/models/")
+def models(models: Models):
+    print("Received:", models.categories)
+    models_data = json.load(open("models_data.json"))
+    out = [x for x in models_data if x["category"] in models.categories]
+    return {"models": out}
+
+
+@ app.post("/model/")
+def model(model: Model):
+    print("Received:", model)
+    models_data = json.load(open("models_data.json"))
+    sub = [x for x in models_data if x["category"] == model.category]
+    out = [x for x in sub[0]["models"] if x["name"] == model.name]
+    return {"model": out}
