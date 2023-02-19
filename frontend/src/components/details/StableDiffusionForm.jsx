f9d7340dd23c697e28e456ea70e3a25dfea6b95a
Philipp John <35657453+johnphilipp@users.noreply.github.com>
2023-02-19 22:26:12 +0100

    small api request bug fix

--- frontend/src/components/details/StableDiffusionForm.jsx
+++ frontend/src/components/details/StableDiffusionForm.jsx
@@ -23,28 +23,18 @@ const StableDiffusionForm = ({
   };
 
   const handleSubmit = (event) => {
-    console.log(process.env.REACT_APP_API_AUTH);
-    console.log(process.env.REACT_APP_MODEL_SD_LOAD_URL);
-
     event.preventDefault();
     setRunResponseIsLoading(true);
 
-    var myHeaders = new Headers();
-    myHeaders.append("Authorization", process.env.REACT_APP_API_AUTH);
-    myHeaders.append("Content-Type", "application/json");
-
-    var requestOptions = {
+    fetch(process.env.REACT_APP_MODEL_SD_RUN_URL, {
       method: "POST",
-      headers: myHeaders,
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: process.env.REACT_APP_API_AUTH,
+      },
       body: JSON.stringify(formData),
-      redirect: "follow",
-    };
-
-    fetch(process.env.REACT_APP_MODEL_SD_RUN_URL, requestOptions)
-      .then((response) => {
-        console.log("loadModel", response);
-        response.json();
-      })
+    })
+      .then((response) => response.json())
       .then((data) => {
         setRunResponseIsLoading(false);
         setRunResponse(data);
