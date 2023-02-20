50e7a1aa7db7440d3dec9dbfa79ed6946f043d8d
Philipp John <35657453+johnphilipp@users.noreply.github.com>
2023-02-20 23:05:21 +0100

    refactored Models page after experiments

--- frontend/src/pages/Models.jsx
+++ frontend/src/pages/Models.jsx
@@ -1,79 +1,11 @@
-import { Container } from "../components/Container";
-import { Fragment, useState } from "react";
+import { useState } from "react";
 import { useQuery } from "@tanstack/react-query";
 import ModelResultsSideBar from "../components/models/ModelResultsSideBar";
 import ModelResultsMain from "../components/models/ModelResultsMain";
 import fetchCategories from "../providers/fetchCategories";
 import fetchModels from "../providers/fetchModels";
-import { Dialog, Disclosure, Menu, Transition } from "@headlessui/react";
-import { XMarkIcon } from "@heroicons/react/24/outline";
-import {
-  ChevronDownIcon,
-  FunnelIcon,
-  MinusIcon,
-  PlusIcon,
-  Squares2X2Icon,
-} from "@heroicons/react/20/solid";
-
-const sortOptions = [
-  { name: "Most Popular", href: "#", current: true },
-  { name: "Best Rating", href: "#", current: false },
-  { name: "Newest", href: "#", current: false },
-  { name: "Price: Low to High", href: "#", current: false },
-  { name: "Price: High to Low", href: "#", current: false },
-];
-const subCategories = [
-  { name: "Totes", href: "#" },
-  { name: "Backpacks", href: "#" },
-  { name: "Travel Bags", href: "#" },
-  { name: "Hip Bags", href: "#" },
-  { name: "Laptop Sleeves", href: "#" },
-];
-const filters = [
-  {
-    id: "color",
-    name: "Color",
-    options: [
-      { value: "white", label: "White", checked: false },
-      { value: "beige", label: "Beige", checked: false },
-      { value: "blue", label: "Blue", checked: true },
-      { value: "brown", label: "Brown", checked: false },
-      { value: "green", label: "Green", checked: false },
-      { value: "purple", label: "Purple", checked: false },
-    ],
-  },
-  {
-    id: "category",
-    name: "Category",
-    options: [
-      { value: "new-arrivals", label: "New Arrivals", checked: false },
-      { value: "sale", label: "Sale", checked: false },
-      { value: "travel", label: "Travel", checked: true },
-      { value: "organization", label: "Organization", checked: false },
-      { value: "accessories", label: "Accessories", checked: false },
-    ],
-  },
-  {
-    id: "size",
-    name: "Size",
-    options: [
-      { value: "2l", label: "2L", checked: false },
-      { value: "6l", label: "6L", checked: false },
-      { value: "12l", label: "12L", checked: false },
-      { value: "18l", label: "18L", checked: false },
-      { value: "20l", label: "20L", checked: false },
-      { value: "40l", label: "40L", checked: true },
-    ],
-  },
-];
-
-function classNames(...classes) {
-  return classes.filter(Boolean).join(" ");
-}
 
 const Models = () => {
-  const [mobileFiltersOpen, setMobileFiltersOpen] = useState(false);
-
   const resultsCategories = useQuery(["categories"], fetchCategories);
   const categories = resultsCategories?.data?.categories ?? [];
   // console.log({ categories });
@@ -99,17 +31,13 @@ const Models = () => {
   };
 
   return (
-    // <main className="-mt-32">
-    //   <div className="mx-auto max-w-7xl px-4 pb-12 sm:px-6 lg:px-8">
-    //     <div className="rounded-lg bg-white px-5 py-6 shadow sm:px-6">
-    //       <h2 className="mb-4 font-light text-2xl">Models</h2>
     <>
       <button
         data-drawer-target="default-sidebar"
         data-drawer-toggle="default-sidebar"
         aria-controls="default-sidebar"
         type="button"
-        class="inline-flex items-center p-2 mt-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+        class="inline-flex items-center p-2 mt-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden focus:outline-none focus:ring-2 focus:ring-gray-200"
       >
         <span class="sr-only">Open sidebar</span>
         <svg
@@ -133,7 +61,7 @@ const Models = () => {
             class="z-40 h-full transition-transform -translate-x-full sm:translate-x-0"
             aria-label="Sidebar"
           >
-            <div className="shadow h-full px-3 py-4 rounded-lg overflow-y-auto bg-slate-200 dark:bg-slate-800">
+            <div className="shadow h-full px-3 py-4 rounded-lg overflow-y-auto bg-slate-200">
               <div className="mb-2 text-lg font-md">Categories:</div>
               <ModelResultsSideBar
                 categories={categories}
@@ -158,9 +86,6 @@ const Models = () => {
         </div>
       </div>
     </>
-    //     </div>
-    //   </div>
-    // </main>
   );
 };
 
