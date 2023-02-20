a8ae04492b0001706cc62dda0c91b8762b5b2654
Philipp John <35657453+johnphilipp@users.noreply.github.com>
2023-02-20 23:11:30 +0100

    changed user icon

--- frontend/src/components/Skeleton.jsx
+++ frontend/src/components/Skeleton.jsx
@@ -5,10 +5,9 @@ import { Link } from "react-router-dom";
 import { Logo } from "./Logo";
 
 const user = {
-  name: "Tom Cook",
-  email: "tom@example.com",
-  imageUrl:
-    "https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
+  name: "Test User",
+  email: "test@user.com",
+  imageUrl: "https://img.icons8.com/fluency/256/name.png",
 };
 
 const navigation = [
